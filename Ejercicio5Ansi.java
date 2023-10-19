c6424532a0cbf40b6e40bb80abe58cbfaabec5c9
15Diego15 <146377686+15Diego15@users.noreply.github.com>
2023-10-19 19:35:54 +0200

    Add files via upload

---
+++ Ejercicio5Ansi.java
@@ -0,0 +1,12 @@
+//primera parte del 5
+public class Ejercicio5Ansi {
+    public static void main(String[] args) {
+        String fila = "";
+        
+        for (int i = 1; i <= 9; i++) {
+            fila += i;
+
+            System.out.printf("%9s\n",fila);
+        }
+    }
+}
